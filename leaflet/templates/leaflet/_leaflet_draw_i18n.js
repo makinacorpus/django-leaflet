15483840e8f4b1f3ca1db84605e119b45dd74876
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-09-26 15:41:09 +0200

    Fix syntax error if form widget translations contains quotes

--- leaflet/templates/leaflet/_leaflet_draw_i18n.js
+++ leaflet/templates/leaflet/_leaflet_draw_i18n.js
@@ -3,51 +3,51 @@ L.drawLocal = {
     draw: {
         toolbar: {
             actions: {
-                title: '{% trans "Cancel drawing" %}',
-                text: '{% trans "Cancel" %}'
+                title: "{% trans "Cancel drawing" %}",
+                text: "{% trans "Cancel" %}"
             },
             buttons: {
-                polyline: '{% trans "Draw a polyline" %}',
-                polygon: '{% trans "Draw a polygon" %}',
-                rectangle: '{% trans "Draw a rectangle" %}',
-                circle: '{% trans "Draw a circle" %}',
-                marker: '{% trans "Draw a marker" %}'
+                polyline: "{% trans "Draw a polyline" %}",
+                polygon: "{% trans "Draw a polygon" %}",
+                rectangle: "{% trans "Draw a rectangle" %}",
+                circle: "{% trans "Draw a circle" %}",
+                marker: "{% trans "Draw a marker" %}"
             }
         },
         handlers: {
             circle: {
                 tooltip: {
-                    start: '{% trans "Click and drag to draw circle." %}'
+                    start: "{% trans "Click and drag to draw circle." %}"
                 }
             },
             marker: {
                 tooltip: {
-                    start: '{% trans "Click map to place marker." %}'
+                    start: "{% trans "Click map to place marker." %}"
                 }
             },
             polygon: {
                 tooltip: {
-                    start: '{% trans "Click to start drawing shape." %}',
-                    cont: '{% trans "Click to continue drawing shape." %}',
-                    end: '{% trans "Click first point to close this shape." %}'
+                    start: "{% trans "Click to start drawing shape." %}",
+                    cont: "{% trans "Click to continue drawing shape." %}",
+                    end: "{% trans "Click first point to close this shape." %}"
                 }
             },
             polyline: {
-                error: '{% trans "<strong>Error:</strong> shape edges cannot cross!" %}',
+                error: "{% trans "<strong>Error:</strong> shape edges cannot cross!" %}",
                 tooltip: {
-                    start: '{% trans "Click to start drawing line." %}',
-                    cont: '{% trans "Click to continue drawing line." %}',
-                    end: '{% trans "Click last point to finish line." %}'
+                    start: "{% trans "Click to start drawing line." %}",
+                    cont: "{% trans "Click to continue drawing line." %}",
+                    end: "{% trans "Click last point to finish line." %}"
                 }
             },
             rectangle: {
                 tooltip: {
-                    start: '{% trans "Click and drag to draw rectangle." %}'
+                    start: "{% trans "Click and drag to draw rectangle." %}"
                 }
             },
             simpleshape: {
                 tooltip: {
-                    end: '{% trans "Release mouse to finish drawing." %}'
+                    end: "{% trans "Release mouse to finish drawing." %}"
                 }
             }
         }
@@ -56,29 +56,29 @@ L.drawLocal = {
         toolbar: {
             actions: {
                 save: {
-                    title: '{% trans "Save changes." %}',
-                    text: '{% trans "Save" %}'
+                    title: "{% trans "Save changes." %}",
+                    text: "{% trans "Save" %}"
                 },
                 cancel: {
-                    title: '{% trans "Cancel editing, discards all changes." %}',
-                    text: '{% trans "Cancel" %}'
+                    title: "{% trans "Cancel editing, discards all changes." %}",
+                    text: "{% trans "Cancel" %}"
                 }
             },
             buttons: {
-                edit: '{% trans "Edit layers" %}',
-                remove: '{% trans "Delete layers" %}'
+                edit: "{% trans "Edit layers" %}",
+                remove: "{% trans "Delete layers" %}"
             }
         },
         handlers: {
             edit: {
                 tooltip: {
-                    text: '{% trans "Drag handles, or marker to edit feature." %}',
-                    subtext: '{% trans "Click cancel to undo changes." %}'
+                    text: "{% trans "Drag handles, or marker to edit feature." %}",
+                    subtext: "{% trans "Click cancel to undo changes." %}"
                 }
             },
             remove: {
                 tooltip: {
-                    text: '{% trans "Click on a feature to remove" %}'
+                    text: "{% trans "Click on a feature to remove" %}"
                 }
             }
         }
