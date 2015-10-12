0e51d859aea77a1891caa19d94dca91c8c9273e3
josenaka <yoshifumi.nakahira@gmail.com>
2015-10-12 23:40:04 +0900

    fixed typo

--- leaflet/templatetags/leaflet_tags.py
+++ leaflet/templatetags/leaflet_tags.py
@@ -40,7 +40,7 @@ def leaflet_js(plugins=None):
     plugin_names = _get_plugin_names(plugins)
     with_forms = PLUGIN_FORMS in plugin_names or PLUGIN_ALL in plugin_names
     FORCE_IMAGE_PATH = app_settings.get('FORCE_IMAGE_PATH')
-    if hasattr(settings,'TEMPLATE_DEBUG')
+    if hasattr(settings,'TEMPLATE_DEBUG'):
         debug = settings.TEMPLATE_DEBUG
     else:
         if settings.TEMPLATES[0]['OPTIONS'].haskey('debug'):
