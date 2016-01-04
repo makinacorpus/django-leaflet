8587d84466521ddb89792807f24b4fb97aad245a
Mathieu Leplatre <mathieu@mozilla.com>
2016-01-04 13:10:06 +0100

    Preparing release 0.18.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.version_info < (2, 7):
 
 setup(
         name='django-leaflet',
-        version='0.17.2.dev0',
+        version='0.18.0',
         author='Mathieu Leplatre',
         author_email='mathieu.leplatre@makina-corpus.com',
         url='https://github.com/makinacorpus/django-leaflet',
