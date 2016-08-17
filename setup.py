803a370c477ae0bd5d7d4c1a572b1c51b212efcd
Gaël UTARD <gael.utard@makina-corpus.com>
2016-08-17 10:41:43 +0200

    Back to development

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.version_info < (2, 7):
 
 setup(
         name='django-leaflet',
-        version='0.18.2',
+        version='0.18.3.dev0',
         author='Mathieu Leplatre',
         author_email='mathieu.leplatre@makina-corpus.com',
         url='https://github.com/makinacorpus/django-leaflet',
