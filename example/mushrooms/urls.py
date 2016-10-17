ea89ecaa93c7cbfa56694a70d2e93c4e8eedda7c
Mathieu Leplatre <mathieu@mozilla.com>
2016-10-17 10:49:15 +0200

    Show map on home

--- example/mushrooms/urls.py
+++ example/mushrooms/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
+from django.conf.urls import url
+from djgeojson.views import GeoJSONLayerView
+
+from .models import MushroomSpot
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^data.geojson$', GeoJSONLayerView.as_view(model=MushroomSpot), name='data')
 ]
