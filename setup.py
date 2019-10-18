99cbf3c1a0f32743df3e6f4a1ea9b31079c02124
Gagaro <gagaro42@gmail.com>
2019-10-18 16:44:30 +0200

    back to dev 0.26.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if sys.version_info < (2, 7):
 
 setup(
         name='django-leaflet',
-        version='0.25.0',
+        version='0.26.0.dev0',
         author='Mathieu Leplatre',
         author_email='mathieu.leplatre@makina-corpus.com',
         url='https://github.com/makinacorpus/django-leaflet',
