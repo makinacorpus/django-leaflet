8346ab716c7e39c982ff77b62de282f8f4cbc7d6
Sébastien Diemer <diemersebastien@yahoo.fr>
2023-06-19 08:50:59 +0200

    make example project use Django 4 (#366)

--- example/mushrooms/urls.py
+++ example/mushrooms/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import url
+from django.urls import path
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
@@ -24,7 +24,8 @@ from .models import MushroomSpot
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
-    url(r'^data.geojson$', GeoJSONLayerView.as_view(model=MushroomSpot, properties=('title', 'description', 'picture_url')), name='data')
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('data.geojson', GeoJSONLayerView.as_view(model=MushroomSpot,
+         properties=('title', 'description', 'picture_url')), name='data')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
