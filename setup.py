0824746141616c44767963694d3f9897c54860a5
Gagaro <gagaro42@gmail.com>
2024-06-17 09:40:00 +0200

    0.30.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(os.path.join(here, 'CHANGES')) as f:
 
 setup(
     name='django-leaflet',
-    version='0.30.1dev',
+    version='0.30.1',
     author='Mathieu Leplatre',
     author_email='mathieu.leplatre@makina-corpus.com',
     url='https://github.com/makinacorpus/django-leaflet',
